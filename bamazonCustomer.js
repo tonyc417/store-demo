adcc1c49e826ddc62ec356807b95f0b7e0a1516c
Tony Cruz <tonycruz@Tonys-MBP.frontierlocal.net>
2019-05-20 22:44:20 -0500

    working on questions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,21 +15,28 @@ connection.connect(function (err) {
     runSearch();
 })
 
-// checkInventory = () => {
-//     connection.query("SELECT * FROM products", (err, res) => {
-//         if (err) throw err;
-//         var testing = res.map(item => item);
-//         console.log(testing);
-//         connection.end();
-//     });
-// }
+checkInventory = () => {
+    connection.query("SELECT * FROM products", (err, res) => {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].product_name + "\t" + 
+            res[i].department_name + "\t" + 
+            res[i].price + "\t" + 
+            res[i].stock_quantity);
+        }
+        connection.end();
+    }); 
+}
 
-runSearch = () => { inquirer.prompt([{
-    type: 'input',
-    name: 'checkInventory',
-    message: 'Would you like to check our inventory?'
-}]).then(function(response) {
-    console.log(response);
+runSearch = () => { inquirer.prompt({
+    name: 'action',
+    type: 'list-input',
+    message: 'What would you like to do?',
+    choices: [
+        'Check the inventory?',
+        'Buy an item?'
+    ]
+}).then(function(choices) {
+    console.log(choices);
     connection.end();
-    console.log("Connection Ended");
 })};
